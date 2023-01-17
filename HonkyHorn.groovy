8b3c20a23857f3e6738fe3eed0167d1d9ee45275
Kevin Harrington <kharrington@softroboticsinc.com>
2023-01-17 14:35:09 -0500

    Saving Bezier

--- HonkyHorn.groovy
+++ HonkyHorn.groovy
@@ -1,1 +1,18 @@
 // code here
+
+import com.neuronrobotics.bowlerstudio.scripting.ScriptingEngine
+import com.neuronrobotics.bowlerkernel.Bezier3d.*;
+import eu.mihosoft.vrl.v3d.*;
+def URL="https://github.com/madhephaestus/HonkyHorn.git"
+def file="bez.json"
+
+//Git stored file loaded but not saved
+BezierEditor editor = new BezierEditor(URL, file,20)
+//Git file loaded and saved. THis will do a git call on each event of change
+//BezierEditor editor = new BezierEditor(URL, file,10)
+
+ArrayList<Transform>  transforms = editor.transforms()
+
+def modelParts = CSG.unionAll(Extrude.hull(new Cube(20).toCSG(), transforms))
+
+return [editor.get(),modelParts]
