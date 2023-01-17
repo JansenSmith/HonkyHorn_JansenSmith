798cd8497fff676ba62cc2fe78cde3c7a16f28c9
Kevin Harrington <kharrington@softroboticsinc.com>
2023-01-17 15:40:04 -0500

    Saving Bezier

--- HonkyHorn.groovy
+++ HonkyHorn.groovy
@@ -8,6 +8,7 @@ def URL="https://github.com/madhephaestus/HonkyHorn.git"
 //Git stored file loaded but not saved
 BezierEditor editor = new BezierEditor(URL, "bez.json",20)
 BezierEditor editor2 = new BezierEditor(URL, "bez2.json",20)
+editor.setStartManip(editor2.getEndManip()) 
 //Git file loaded and saved. THis will do a git call on each event of change
 //BezierEditor editor = new BezierEditor(URL, file,10)
 
